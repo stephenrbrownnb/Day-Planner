a597c5e6d42f7b2b412296a4d93a524778439ccf
Stephen Brown <stephenrbrownnb@gmail.com>
2022-11-23 19:03:06 -0400

    Added local storage

--- script.js
+++ script.js
@@ -10,22 +10,42 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
 
 //Determines current hour
 var hourLive = today.$H;
-//loop to go through timeslots and compare to current time
-for (var i = 0; i < $(".time-block").length; i++) {
-let currentTimeBlock = $(".time-block")[i];
-let currentTime = currentTimeBlock.id;
 
-if (currentTime < hourLive) {
-  $(currentTimeBlock).addClass("past");
+//loop to go through timeslots and compare to current time, as well as get the local storage 
+
+
+$(".time-block").each(function(){
+
+  let currentTimeSlot=$(this).attr("id");
+ $(this).children(".description").val(localStorage.getItem(currentTimeSlot));
+
+ if (currentTimeSlot < hourLive) {
+  $(this).addClass("past");
   }
-  if (currentTime == hourLive) {
-    $(currentTimeBlock).addClass("present");
+  if (currentTimeSlot == hourLive) {
+    $(this).addClass("present");
     }
-    if (currentTime > hourLive) {
-      $(currentTimeBlock).addClass("future");
-      }
+    if (currentTimeSlot > hourLive) {
+      $(this).addClass("future");
+      
+}})
+//Action for saving a timeslot
+$(".saveBtn").click(function(){
+//Sets a variable for which button was hit
+  let currentButton = $(this);
+ //Retrieves the text from the textbox and the name of the timeslot.
+  var userInput = $(currentButton).siblings(".description").val();
+  var timeOfDay = $(currentButton).parent().attr("id");
+//stores the text to local storage under the time of day.
+   localStorage.setItem(timeOfDay, userInput);
+})
 
-}
+
+
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
